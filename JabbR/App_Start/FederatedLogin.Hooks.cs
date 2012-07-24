f8585488aee548f867b3c8b62e75cb1d0167310a
Brandon Werner <brandon.werner@microsoft.com>
2012-07-24 01:14:53 -0700

    Fixed Claim parsing issue with Name
    Name does not exist in the Claims returned from ACS. You ned to pull the
    username from the Claim through looking at other data in the Claim.
    Fixed #541

--- JabbR/App_Start/FederatedLogin.Hooks.cs
+++ JabbR/App_Start/FederatedLogin.Hooks.cs
@@ -9,6 +9,9 @@ namespace JabbR.App_Start
 {
     public static partial class FederatedLogin
     {
+
+        
+
         private partial class NoConfigWSFederationAuthenticationModule 
         {
             protected override void OnSignedIn(EventArgs args)
@@ -21,6 +24,7 @@ namespace JabbR.App_Start
             public void ProcessRequest(HttpContext context)
             {
                 IClaimsIdentity identity = context.User.Identity as IClaimsIdentity;
+                string _domain = "", _email = "", _firstname="", _lastname="", _strClaimType;
 
                 if (identity == null)
                 {
@@ -33,14 +37,30 @@ namespace JabbR.App_Start
                     throw new InvalidOperationException("NameIdentifier claim not found.");
                 }
 
-                if (identity.Name == null)
-                {
-                    throw new InvalidOperationException("Name claim not found.");
-                }
+                // This runs through and forms the data for the claim. Obviously this can be done with Linq
+               foreach (var c in identity.Claims)
+            {
+                _strClaimType = c.ClaimType;
+                if (_strClaimType.EndsWith("domain"))
+                    _domain = c.Value;
+                if (_strClaimType.EndsWith("EmailAddress"))
+                    _email = c.Value;
+                if (_strClaimType.EndsWith("FirstName"))
+                      _firstname = c.Value;
+                if (_strClaimType.EndsWith("LastName"))
+                       _lastname = c.Value ;
+            }
+
+            // Name does not exist in the Claim coming from ACS at this point in time
+
+             //   if (identity.Name == null)
+             //   {
+             //       throw new InvalidOperationException("Name claim not found.");
+             //   }
 
                 string userIdentity = userIdentityClaim.Value;
-                string username = identity.Name;
-                string email = String.Empty;
+                string username = _firstname + "" + _lastname;
+                string email = _email;
                 Claim emailClaim = identity.Claims.SingleOrDefault(c => c.ClaimType == ClaimTypes.Email);
                 if (emailClaim != null)
                 {
